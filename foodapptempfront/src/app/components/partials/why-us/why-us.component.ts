1d0ac7b356eaf3b4cbe9b1d41eed9ad63069eb00
bhavitagadara <168505737+bhavitagadara@users.noreply.github.com>
2025-05-13 11:10:49 +0530

    Add files via upload

---
+++ foodapptempfront/src/app/components/partials/why-us/why-us.component.ts
@@ -0,0 +1,19 @@
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import AOS from 'aos';
+@Component({
+  selector: 'app-why-us',
+  standalone: true,
+  imports: [],
+  templateUrl: './why-us.component.html',
+  styleUrl: './why-us.component.css'
+})
+export class WhyUsComponent implements OnInit{
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init(); // Safe browser-only usage
+    }
+  }
+}
