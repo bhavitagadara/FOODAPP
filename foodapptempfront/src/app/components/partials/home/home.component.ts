5f071fe65a9f81b539be0581689c7228ef0ea013
bhavitagadara <168505737+bhavitagadara@users.noreply.github.com>
2025-05-13 11:01:55 +0530

    Add files via upload

---
+++ foodapptempfront/src/app/components/partials/home/home.component.ts
@@ -0,0 +1,24 @@
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import AOS from 'aos'; 
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent  implements AfterViewInit {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Only initialize AOS on the browser side
+      import('aos').then(AOS => {
+        AOS.init();
+      });
+    }
+  }
+}
